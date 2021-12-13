a0e576c22d236e05f4b99541d11fb50c04c20c73
Nika <ndogh13@freeuni.edu.ge>
2021-12-13 17:43:53 +0400

    Update mean split improvement tests to use sample input

--- tests/testthat/test-mean_split_improvement.R
+++ tests/testthat/test-mean_split_improvement.R
@@ -1,8 +1,4 @@
 test_that("random example works", {
-  set.seed(49)
-
-  X <- matrix(runif(50*5), 50, 5)
-  Y <- matrix(runif(50*2), 50, 2)
   m_feature = 5
   min_leaf = 5
   n_trees = 20
@@ -11,27 +7,19 @@ test_that("random example works", {
   res <- MeanSplitImprovement(X, Y, sample_size, n_trees, m_feature, min_leaf)
 
   expect_length(res, ncol(X))
-  expect_equal(res, c(1.458, 2.965, 3.947, 1.175, 1.037), tolerance=1e-3)
+  expect_equal(res, c(2.08, 2.76, 3.72, 1.22, 1.54), tolerance=1e-3)
 })
 
 
 test_that("default values work", {
-  set.seed(49)
-
-  X <- matrix(runif(50*5), 50, 5)
-  Y <- matrix(runif(50*2), 50, 2)
   res <- MeanSplitImprovement(X, Y)
 
   expect_length(res, ncol(X))
-  expect_equal(res, c(0.807, 2.891, 3.460, 0.623, 0.514), tolerance=1e-3)
+  expect_equal(res, c(0.71, 3.27, 3.61, 0.73, 0.58), tolerance=1e-3)
 })
 
 
 test_that("f-test parameter works", {
-  set.seed(49)
-
-  X <- matrix(runif(50*5), 50, 5)
-  Y <- matrix(runif(50*2), 50, 2)
   m_feature = 5
   min_leaf = 5
   n_trees = 20
@@ -40,5 +28,5 @@ test_that("f-test parameter works", {
 
   res <- MeanSplitImprovement(X, Y, sample_size, n_trees, m_feature, min_leaf, alpha_threshold)
   expect_length(res, ncol(X))
-  expect_equal(res, c(1.052, 2.776, 3.662, 0.721, 0.877), tolerance=1e-3)
+  expect_equal(res, c(1.38, 3.00, 3.64, 1.50, 0.82), tolerance=1e-3)
 })
