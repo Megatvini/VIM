65af8671beb46d3710b1dff1074db666a061eeec
Nika <ndogh13@freeuni.edu.ge>
2021-12-14 17:00:52 +0400

    Make mean split tests uniform

--- tests/testthat/test-mean_split_improvement.R
+++ tests/testthat/test-mean_split_improvement.R
@@ -1,32 +1,33 @@
 test_that("random example works", {
   m_feature = 5
-  min_leaf = 5
+  min_leaf = 10
   n_trees = 20
-  sample_size = 20
+  sample_size = 16
 
+  set.seed(42)
   res <- MeanSplitImprovement(X, Y, sample_size, n_trees, m_feature, min_leaf)
-
   expect_length(res, ncol(X))
-  expect_equal(res, c(2.08, 2.76, 3.72, 1.22, 1.54), tolerance=1e-3)
+  expect_equal(res, c(0.681, 2.295, 1.559, 0.124, 0.091), tolerance=1e-3)
 })
 
 
 test_that("default values work", {
+  set.seed(42)
   res <- MeanSplitImprovement(X, Y)
-
   expect_length(res, ncol(X))
-  expect_equal(res, c(0.71, 3.27, 3.61, 0.73, 0.58), tolerance=1e-3)
+  expect_equal(res, c(0.523, 3.195, 3.187, 0.816, 0.545), tolerance=1e-3)
 })
 
 
 test_that("f-test parameter works", {
   m_feature = 5
-  min_leaf = 5
+  min_leaf = 10
   n_trees = 20
-  sample_size = 20
+  sample_size = 16
   alpha_threshold = 0.2
 
+  set.seed(42)
   res <- MeanSplitImprovement(X, Y, sample_size, n_trees, m_feature, min_leaf, alpha_threshold)
   expect_length(res, ncol(X))
-  expect_equal(res, c(1.38, 3.00, 3.64, 1.50, 0.82), tolerance=1e-3)
+  expect_equal(res, c(0.512, 2.277, 1.559, 0.104, 0.000), tolerance=1e-3)
 })
